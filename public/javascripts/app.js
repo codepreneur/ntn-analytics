4977dee418e154edd3fd6e7be02ef8df6a9d81d1
gomenasaai <contramundee@gmail.com>
2014-09-28 23:40:50 +0100

    configured require.js and bootstraped application

---
+++ public/javascripts/app.js
@@ -0,0 +1,7 @@
+define(['ember', 'ember-data', 'nvd3', 'jquery-ui'], function(Ember){
+
+	var App = Ember.Application.create();
+
+	return App;
+
+});
---
+++ public/javascripts/require.config.js
@@ -0,0 +1,32 @@
+require.config({
+	baseUrl: 'javascripts',
+	paths: {
+		'jquery': 'vendor/jquery.min',
+		'jquery-ui': 'vendor/jquery-ui.min',
+		'd3': 'vendor/d3.v3.min',
+		'nvd3': 'vendor/nv.d3',
+		'hbs': 'vendor/handlebars-v1.3.0',
+		'ember': 'vendor/ember',
+		'ember-data': 'vendor/ember-data',
+	},
+
+	shim: {
+		'ember': {
+			deps: ['jquery', 'hbs'],
+			exports: 'Ember'
+		},
+
+		'ember-data': ['ember'],
+		'nvd3': ['d3'],
+		'jquery-ui': ['jquery'],
+		'd3': {
+			exports: 'd3'
+		}
+	}
+});
+
+// Initialise require to load the app
+
+require.require({
+
+});
